e7c81ed0b696d07e485ee789129bded0ea2a73f1
isabellecwy <146903288+isabellecwy@users.noreply.github.com>
2024-03-19 09:33:03 +0800

    Add files via upload

---
+++ Mygame/game.js
@@ -0,0 +1,26 @@
+var config = {
+    type: Phaser.AUTO,
+    ////// pixel size * tile map size 
+    width: 32 * 20,
+    height: 32 * 20,
+    /////////////////////////////////////////
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    // scale: {
+    //     mode: Phaser.Scale.FIT,
+    //     autoCenter: Phaser.Scale.CENTER_BOTH
+    // },
+    backgroundColor: '#000000',
+    pixelArt: true,
+    //// Add all scenes below in the array
+    scene: [ level1, lvl2,  level3, level4 ]
+};
+
+var game = new Phaser.Game(config);
+
+
+
---
+++ Mygame/lvl2.js
@@ -0,0 +1,258 @@
+
+class lvl2 extends Phaser.Scene {
+    constructor ()
+    {
+        super({ key: 'lvl2' });
+    }
+
+    preload() {
+
+        // Step 1, load JSON
+        this.load.tilemapTiledJSON("world2", "map/lvl2.tmj");
+
+        this.load.spritesheet("main", "character/character1.png", {
+            frameWidth: 64,
+            frameHeight: 64,
+          });
+
+          this.load.spritesheet("enemy2", "character/villain_kaisa.png", {
+            frameWidth: 64,
+            frameHeight: 64,
+          });
+
+        
+
+        // Step 2 : Preload any images here
+        this.load.image("014img", "assets/014.png");
+        this.load.image("03img", "assets/03.png");
+        this.load.image("04img", "assets/04.png");
+        this.load.image("06img", "assets/06.png");
+        this.load.image("TileAndStoneimg", "assets/TileAndStone.png");
+        this.load.image("key1", "assets/key.png");
+        this.load.image("key2", "assets/key2.png");
+        this.load.image("key4", "assets/key4.png");
+        this.load.image("key3", "assets/key3.png");
+        
+       
+        
+    } // end of preload //
+
+    create (){
+
+    console.log("lvl2")
+
+    //Step 3 - Create the map from main
+    let map = this.make.tilemap({ key: "world2" });
+    
+
+
+    
+    // Step 4 Load the game tiles
+    // 1st parameter is name in Tiled,
+    // 2nd parameter is key in Preload
+    let Tiles014 = map.addTilesetImage("014", "014img");
+    let Tiles03 = map.addTilesetImage("03", "03img");
+    let Tiles04 = map.addTilesetImage("04", "04img");
+    let Tiles06 = map.addTilesetImage("06", "06img");
+    let TileAndStone = map.addTilesetImage("TileAndStone", "TileAndStoneimg");
+    
+
+
+
+    //Step 5  create an array of tiles
+    let tilesArray = [
+       Tiles014, Tiles03, Tiles04, Tiles06,  TileAndStone,
+    ]
+
+    // Step 6  Load in layers by layers
+    this.floorLayer = map.createLayer("floorLayer",tilesArray,0,0);
+    this.wallLayer = map.createLayer("wallLayer",tilesArray,0,0);
+    this.decoLayer = map.createLayer("decoLayer",tilesArray,0,0);
+    this.furnitureLayer = map.createLayer("furnitureLayer",tilesArray,0,0);
+    this.cctvLayer = map.createLayer("cctvLayer",tilesArray,0,0);
+    this.wallLayer2 = map.createLayer("wallLayer2",tilesArray,0,0);
+
+    //load object
+    let enemy2 = map.findObject("objectLayer", (obj) => obj.name === "enemy2");
+    let key1 = map.findObject("objectLayer", (obj) => obj.name === "key1");
+    let key2 = map.findObject("objectLayer", (obj) => obj.name === "key2");
+    let key3 = map.findObject("objectLayer", (obj) => obj.name === "key3");
+    let key4 = map.findObject("objectLayer", (obj) => obj.name === "key4");
+
+
+    //My Character
+    this.anims.create({
+        key: "main-up",
+        frames: this.anims.generateFrameNumbers("main", {
+          start: 105,
+          end: 112,
+        }),
+        frameRate: 5,
+        repeat: -1,
+      });
+  
+      this.anims.create({
+        key: "main-down",
+        frames: this.anims.generateFrameNumbers("main", {
+          start: 131,
+          end: 138,
+        }),
+        frameRate: 5,
+        repeat: -1,
+      });
+  
+      this.anims.create({
+        key: "main-left",
+        frames: this.anims.generateFrameNumbers("main", {
+          start: 118,
+          end: 125,
+        }),
+        frameRate: 5,
+        repeat: -1,
+      });
+  
+      this.anims.create({
+        key: "main-right",
+        frames: this.anims.generateFrameNumbers("main", {
+          start: 144,
+          end: 151,
+        }),
+        frameRate: 5,
+        repeat: -1,
+      });
+  
+      this.player = this.physics.add.sprite(320, 133, "main").play("main-down");
+      window.player = this.player;
+
+      this.player.body.setSize(this.player.width * 0.8, this.player.height * 0.8)
+
+      // this.floorLayer.setCollisionByExclusion(-1, true);
+      this.decoLayer.setCollisionByExclusion(-1, true);
+      this.furnitureLayer.setCollisionByExclusion(-1, true);
+      this.cctvLayer.setCollisionByExclusion(-1, true);
+      this.wallLayer.setCollisionByExclusion(-1, true);
+  
+      // this.physics.add.collider(this.player, this.floorLayer);
+      this.physics.add.collider(this.player, this.decoLayer);
+      this.physics.add.collider(this.player, this.cctvLayer);
+      this.physics.add.collider(this.player, this.wallLayer);
+      this.physics.add.collider(this.player, this.furnitureLayer);
+      
+   
+      this.cursors = this.input.keyboard.createCursorKeys();
+    
+
+     this.cameras.main.startFollow(this.player);
+
+     this.enemy2 = this.physics.add.sprite(enemy2.x, 545, "enemy2");
+     this.key1 = this.physics.add.sprite(key1.x, key1.y, "key1");
+     this.key2 = this.physics.add.sprite(key2.x, key2.y, "key2");
+     this.key3 = this.physics.add.sprite(key3.x, key3.y, "key3");
+     this.key4 = this.physics.add.sprite(key4.x, key4.y, "key4");
+
+     this.physics.add.overlap(this.player, this.enemy2, this.hitEnemy2, null, this);
+     this.physics.add.overlap(this.player, this.key1, this.hitKey1, null, this);
+     this.physics.add.overlap(this.player, this.key2, this.hitKey2, null, this);
+     this.physics.add.overlap(this.player, this.key3, this.hitKey3, null, this);
+     this.physics.add.overlap(this.player, this.key4, this.hitKey4, null, this);
+
+
+
+     this.tweens.add({
+      targets: this.enemy2,
+      y: 200,
+      flipX: true,
+      yoyo: true,
+      duration: 3000,
+      repeat: -1
+    })
+     
+ 
+
+    // var level1Down = this.input.keyboard.addKey(49);
+    
+    // level1Down.on('down', function(){
+    //    console.log("49 pressed, jump to level 1");
+    //        this.scene.start("level1");
+    //    }, this );
+    
+
+    
+
+
+    
+
+     
+
+    } // end of create //
+
+    update () {
+      if (
+        this.player.x > 302 &&
+        this.player.x < 345 &&
+        this.player.y < 89
+      ) {
+        console.log("Door2");
+        this.room2();
+      }
+
+      
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play("main-left", true);
+          } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play("main-right", true);
+          } else if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-160);
+            this.player.anims.play("main-up", true);
+          } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(160);
+            this.player.anims.play("main-down", true);
+          } else {
+            this.player.setVelocity(0);
+            this.player.anims.stop();
+          }
+
+    } // end of update // 
+
+    //outside of update
+
+  // Function to jump to room1
+  room2(player, tile) {
+    console.log("room2 function");
+    this.scene.start("level3",);
+  }
+ 
+
+  hitEnemy2(player, item) {
+    console.log("Ouch!!!");
+    this.cameras.main.shake(200);
+    // return false;
+  }
+
+  hitKey1(player, item) {
+    console.log("Key1");
+    // this.cameras.main.shake(200);
+    item.disableBody(true, true); // remove fire
+  }
+
+  hitKey2(player, item) {
+    console.log("Key2");
+    // this.cameras.main.shake(200);
+    item.disableBody(true, true); // remove fire
+  }
+
+  hitKey3(player, item) {
+    console.log("Key3");
+    // this.cameras.main.shake(200);
+    item.disableBody(true, true); // remove fire
+  }
+
+  hitKey4(player, item) {
+    console.log("Key4");
+    // this.cameras.main.shake(200);
+    item.disableBody(true, true); // remove fire
+  }
+}
